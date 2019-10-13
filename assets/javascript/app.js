1400e6005943349597a5eaced96ff2d65278fcd0
Joaquin Valle <jv346@cornell.edu>
2019-10-13 16:56:54 -0500

    fixed color changing when samebutton is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,17 +11,8 @@ $(document).ready(function() {
         $("#initial-buttons").append(button)
     }
 
-    $("#submit").on("click", function() {
-
-    event.preventDefault()
-    var newFruit = $("#add-a-fruit").val()
-    let button = $("<button>")
-    $(button).attr("id", newFruit)
-    $(button).addClass("fruit-button rounded-pill mr-1 mb-2 btn-outline-primary")
-    button.text(newFruit)
-    $("#initial-buttons").append(button)
-    fruitButtonClick()
-    })
+    var colorArr = ["btn-outline-danger", "btn-outline-success", "btn-outline-info", "btn-outline-warning", "btn-outline-dark"]
+    var colorCounter = 1
 
     function fruitButtonClick() {
 
@@ -29,28 +20,40 @@ $(document).ready(function() {
         $(".fruit-button").on("click", function() {
 
             if ($(this).hasClass("clicked")) {
-                $(this).removeClass("btn-outline-danger")
-                $(this).addClass("btn-danger")
-                clickCounter = clickCounter + 10
+                if (colorCounter > 4 ) {
+                    colorCounter = 0
+                    for (i = 0; i < colorArr.length; i++) {
+                        $(".fruit-button").removeClass(colorArr[i])
+                    }
+                }
+                var currentColor = colorArr[colorCounter]
+                $(this).removeClass("btn-outline-danger " + currentColor)
+                $(this).addClass(currentColor)
+                console.log(colorArr[colorCounter])
+                colorCounter++
+                clickCounter += 10
                 var fruit = $(this).attr("id")
                 getAPI(clickCounter)
+                console.log(colorCounter)
             }
 
             else {
                 clickCounter = 0
+                colorCounter = 0
                 $("#results").text("")
-                $(".fruit-button").removeClass("btn-outline-danger btn-danger clicked")
+                $(".fruit-button").removeClass("btn-outline-danger clicked")
+                for (i = 0; i < colorArr.length; i++) {
+                    $(".fruit-button").removeClass(colorArr[i])
+                }
                 $(".fruit-button").addClass("btn-outline-primary")
 
                 $(this).removeClass("btn-outline-primary")
                 $(this).addClass("btn-outline-danger clicked")
                 var fruit = $(this).attr("id")
                 getAPI(clickCounter)
+                colorCounter = 1
             }
 
-
-            
-            
             function getAPI(offset){
                 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=xtOTCHmJ2wr5zCnplnBlO3wLiH97K3kv&q=&limit=10&offset=" + offset + "&rating=G&lang=en&q=" + fruit
     
@@ -77,6 +80,19 @@ $(document).ready(function() {
             }
         })
     }
+
+    $("#submit").on("click", function() {
+
+        event.preventDefault()
+        var newFruit = $("#add-a-fruit").val()
+        let button = $("<button>")
+        $(button).attr("id", newFruit)
+        $(button).addClass("fruit-button rounded-pill mr-1 mb-2 btn-outline-primary")
+        button.text(newFruit)
+        $("#initial-buttons").append(button)
+        fruitButtonClick()
+        })
+
     fruitButtonClick()
 });
 
